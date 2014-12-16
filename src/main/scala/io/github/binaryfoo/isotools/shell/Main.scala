81d588dbbabcaac8b42e9fe91675724018bdd6bb
Will Currie <will@currie.id.au>
2014-12-17 06:11:16 +1100

    Plug another memory leak in Main.
    Not feeling so enthused about Streams any more.

--- src/main/scala/io/github/binaryfoo/isotools/shell/Main.scala
+++ src/main/scala/io/github/binaryfoo/isotools/shell/Main.scala
@@ -2,67 +2,70 @@ package io.github.binaryfoo.isotools.shell
 
 import io.github.binaryfoo.isotools.MsgPair.RichEntryIterable
 import io.github.binaryfoo.isotools.shell.FieldFilter.MatchOp
-import io.github.binaryfoo.isotools.{ConvertibleToMap, LogReader}
+import io.github.binaryfoo.isotools.{LogEntry, ConvertibleToMap, LogReader}
 import scopt.Read
+
+import scala.io.Source
 
 object Main extends App {
 
   Options.parse(args).map { config =>
-    def logEntries = LogReader(config.strict).readFilesOrStdIn(config.input)
-
-    def pipeline: Stream[ConvertibleToMap] = {
-      var entries: Stream[ConvertibleToMap] = if (config.pair)
-        logEntries.pair()
-      else
-        logEntries
-
-      if (config.header) {
-        config.format.header().map(println(_))
-      }
-
-      if (config.filters.nonEmpty) {
-        entries = entries.applyFilters(config.filters, config.beforeContext, config.afterContext)
-      }
-
-      // presumably both these options prevent incremental output
-      if (config.sortBy != null) {
-        entries = entries.sortBy(_(config.sortBy))
-        if (config.sortDescending) {
-          entries = entries.reverse
-        }
-      }
-      entries
+    if (config.header) {
+      config.format.header().map(println(_))
     }
-
-    pipeline
+    val pipeline = new Pipeline(config)
+    pipeline()
       .map(e => config.format(e))
       .foreach(e => println(e))
   }
+}
 
-  implicit class RichLogEntries(val v: Stream[ConvertibleToMap]) extends AnyVal {
+class Pipeline(val config: Config) {
+  
+  def apply(): Stream[ConvertibleToMap] = {
+    sort(filter(pair(read()).toIterator))
+  }
 
-    def applyFilters(filters: Seq[LogFilter], beforeContext: Int, afterContext: Int): Stream[ConvertibleToMap] = {
-      val shouldInclude = (m: ConvertibleToMap) => filters.forall(_(m))
-      if (beforeContext == 0 && afterContext == 0) {
-        // premature optimization for this case?
-        v.filter(shouldInclude)
-      } else {
-        val preceding = new BoundedQueue[ConvertibleToMap](beforeContext)
-        var aftersNeeded = 0
-        v.flatMap { item =>
-          if (shouldInclude(item)) {
-            aftersNeeded = afterContext
-            preceding.dump() :+ item
-          } else if (aftersNeeded > 0) {
-            aftersNeeded -= 1
-            List(item)
-          } else {
-            preceding.add(item)
-            List.empty
-          }
+  def read() = LogReader(config.strict).readFilesOrStdIn(config.input)
+
+  def pair(v: Stream[LogEntry]): Stream[ConvertibleToMap] = if (config.pair) v.pair() else v
+
+  // need an Iterator instead of Stream to prevent a call to filter() or flatMap() pinning the whole stream
+  // in memory until the first match (if any)
+  def filter(v: Iterator[ConvertibleToMap]): Iterator[ConvertibleToMap] = {
+    val shouldInclude = (m: ConvertibleToMap) => config.filters.forall(_(m))
+
+    if (config.filters.isEmpty) {
+      v
+    } else if (config.beforeContext == 0 && config.afterContext == 0) {
+      // premature optimization for this case?
+      v.filter(shouldInclude)
+    } else {
+      val preceding = new BoundedQueue[ConvertibleToMap](config.beforeContext)
+      var aftersNeeded = 0
+      v.flatMap { item =>
+        if (shouldInclude(item)) {
+          aftersNeeded = config.afterContext
+          preceding.dump() :+ item
+        } else if (aftersNeeded > 0) {
+          aftersNeeded -= 1
+          List(item)
+        } else {
+          preceding.add(item)
+          List.empty
         }
       }
     }
   }
 
+  // not always going to work in a bounded amount of memory
+  def sort(v: Iterator[ConvertibleToMap]): Stream[ConvertibleToMap] = {
+    if (config.sortBy == null) {
+      v.toStream
+    } else if (config.sortDescending) {
+      v.toStream.sortBy(_(config.sortBy)).reverse
+    } else {
+      v.toStream.sortBy(_(config.sortBy))
+    }
+  }
 }
