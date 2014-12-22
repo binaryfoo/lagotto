1394cb3998e11b6a56f582a8ffc102f5d4edaaa5
Will Currie <will@currie.id.au>
2014-12-23 06:47:44 +1100

    Add Stream.tap().

--- src/main/scala/io/github/binaryfoo/lagotto/package.scala
+++ src/main/scala/io/github/binaryfoo/lagotto/package.scala
@@ -4,4 +4,12 @@ package object lagotto {
 
   type LogFieldExpr = LogLike => String
 
+  implicit class TappableStream[A](val s: Stream[A]) extends AnyVal {
+    def tap(f: A => Unit): Stream[A] = {
+      s.map { e =>
+        f(e)
+        e
+      }
+    }
+  }
 }
