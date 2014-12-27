982efa2b67baa9a5f0f7d48562ca03d7b496d24a
Will Currie <will@currie.id.au>
2014-12-28 08:14:36 +1100

    Yak shave: de-duplication.

--- src/test/scala/io/github/binaryfoo/lagotto/shell/MainTest.scala
+++ src/test/scala/io/github/binaryfoo/lagotto/shell/MainTest.scala
@@ -1,14 +1,15 @@
 package io.github.binaryfoo.lagotto.shell
 
-import java.io.{File, ByteArrayOutputStream}
+import java.io.ByteArrayOutputStream
 
-import io.github.binaryfoo.lagotto.LogReader
 import org.scalatest.{FlatSpec, Matchers}
 
 class MainTest extends FlatSpec with Matchers {
 
+  def testFile(f: String): String = s"src/test/resources/$f"
+
   "Main" should "produce tab separated output with --tsv option" in {
-    val output = run("--tsv", "time,mti,11", "src/test/resources/a-bunch.xml")
+    val output = run("--tsv", "time,mti,11", testFile("a-bunch.xml"))
     output shouldEqual """time	mti	11
                          |00:00:03.292	0200	1
                          |00:00:04.292	0200	2
@@ -18,7 +19,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "produce jira formatted table with --jira-table option" in {
-    val output = run("--jira-table", "time,mti,11", "src/test/resources/a-bunch.xml")
+    val output = run("--jira-table", "time,mti,11", testFile("a-bunch.xml"))
     output shouldEqual """||time||mti||11||
 |00:00:03.292|0200|1|
 |00:00:04.292|0200|2|
@@ -28,7 +29,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "produce a HTML formatted table with --html option" in {
-    val output = run("--html", "time,mti,11", "src/test/resources/a-bunch.xml")
+    val output = run("--html", "time,mti,11", testFile("a-bunch.xml"))
     output should include ("<table>")
     output should include ("<tr><th>time</th><th>mti</th><th>11</th></tr>")
     output should include ("<tr><td>00:00:03.292</td><td>0200</td><td>1</td></tr>")
@@ -36,7 +37,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "include delay between messages with delay field in --csv option" in {
-    val output = run("-f", "socket=10.0.0.1:4321", "--csv", "time,mti,11,delay", "src/test/resources/a-bunch.xml")
+    val output = run("-f", "socket=10.0.0.1:4321", "--csv", "time,mti,11,delay", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,delay
                          |00:00:03.292,0200,1,0
                          |00:00:04.992,0210,1,1700
@@ -44,14 +45,14 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "allow filtering on delay between messages" in {
-    val output = run("-f", "socket=10.0.0.1:4321", "-f", "delay>1000", "--csv", "time,mti,11,delay", "src/test/resources/a-bunch.xml")
+    val output = run("-f", "socket=10.0.0.1:4321", "-f", "delay>1000", "--csv", "time,mti,11,delay", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,delay
                          |00:00:04.992,0210,1,1700
                          |""".stripMargin
   }
 
   it should "allow sort on delay between messages" in {
-    val output = run("-f", "socket=10.0.0.1:4321", "--csv", "time,mti,11,delay", "--sort-desc", "delay", "src/test/resources/a-bunch.xml")
+    val output = run("-f", "socket=10.0.0.1:4321", "--csv", "time,mti,11,delay", "--sort-desc", "delay", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,delay
                          |00:00:04.992,0210,1,1700
                          |00:00:03.292,0200,1,0
@@ -59,7 +60,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "group rows when count field included in --csv option" in {
-    val output = run("--csv", "time(mm:ss),count", "src/test/resources/a-bunch.xml")
+    val output = run("--csv", "time(mm:ss),count", testFile("a-bunch.xml"))
     output shouldEqual """time(mm:ss),count
                           |00:03,1
                           |00:04,3
@@ -67,21 +68,21 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "filter on group count with --field count>N" in {
-    val output = run("--csv", "time(mm:ss),count", "--field", "count>2", "src/test/resources/a-bunch.xml")
+    val output = run("--csv", "time(mm:ss),count", "--field", "count>2", testFile("a-bunch.xml"))
     output shouldEqual """time(mm:ss),count
                           |00:04,3
                           |""".stripMargin
   }
 
   it should "filter on group count(condition) with --field count>N" in {
-    val output = run("--csv", "time(mm:ss),count(time(mm:ss)=00:03)", "--field", "count(time(mm:ss)=00:03)>1", "src/test/resources/a-bunch.xml")
+    val output = run("--csv", "time(mm:ss),count(time(mm:ss)=00:03)", "--field", "count(time(mm:ss)=00:03)>1", testFile("a-bunch.xml"))
     output shouldEqual """time(mm:ss),count(time(mm:ss)=00:03)
                           |00:03,1
                           |""".stripMargin
   }
 
   it should "count only if condition is true for count(39=01) in --csv option" in {
-    val output = run("--csv", "time(mm:ss),count(39=01),count(39=00)", "src/test/resources/a-bunch.xml")
+    val output = run("--csv", "time(mm:ss),count(39=01),count(39=00)", testFile("a-bunch.xml"))
     output shouldEqual """time(mm:ss),count(39=01),count(39=00)
                           |00:03,0,0
                           |00:04,1,1
@@ -89,7 +90,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "support mysql like group_concat() in --csv option" in {
-    val output = run("--csv", "time(mm:ss),group_concat(48.1(/.*#//))", "src/test/resources/a-bunch.xml")
+    val output = run("--csv", "time(mm:ss),group_concat(48.1(/.*#//))", testFile("a-bunch.xml"))
     output shouldEqual """time(mm:ss),group_concat(48.1(/.*#//))
                          |00:03,1
                          |00:04,2,3,4
@@ -97,7 +98,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "allow sort on count field included in --csv option" in {
-    val output = run("--csv", "time(mm:ss),count", "--sort-desc", "count", "src/test/resources/a-bunch.xml")
+    val output = run("--csv", "time(mm:ss),count", "--sort-desc", "count", testFile("a-bunch.xml"))
     output shouldEqual """time(mm:ss),count
                          |00:04,3
                          |00:03,1
@@ -105,7 +106,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "group rows when max(lifespan) field included in --csv option" in {
-    val output = run("--csv", "ipAddress,max(lifespan)", "src/test/resources/a-pair.xml")
+    val output = run("--csv", "ipAddress,max(lifespan)", testFile("a-pair.xml"))
     output shouldEqual """ipAddress,max(lifespan)
                           |10.0.0.1,10005
                           |,
@@ -113,7 +114,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "group rows when max(lifespan) field included in --jira-table option" in {
-    val output = run("--jira-table", "ipAddress,max(lifespan)", "src/test/resources/a-pair.xml")
+    val output = run("--jira-table", "ipAddress,max(lifespan)", testFile("a-pair.xml"))
     output shouldEqual """||ipAddress||max(lifespan)||
 |10.0.0.1|10005|
 |||
@@ -121,7 +122,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "group rows when min(lifespan) field included in --csv option" in {
-    val output = run("--csv", "ipAddress,min(lifespan)", "src/test/resources/a-pair.xml")
+    val output = run("--csv", "ipAddress,min(lifespan)", testFile("a-pair.xml"))
     output shouldEqual """ipAddress,min(lifespan)
                           |10.0.0.1,1000
                           |,
@@ -129,7 +130,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "group rows when sum(lifespan) field included in --csv option" in {
-    val output = run("--csv", "ipAddress,sum(lifespan)", "src/test/resources/a-pair.xml")
+    val output = run("--csv", "ipAddress,sum(lifespan)", testFile("a-pair.xml"))
     output shouldEqual """ipAddress,sum(lifespan)
                           |10.0.0.1,11005
                           |,
@@ -137,14 +138,14 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "leave out the header with --no-header option" in {
-    val output = run("--no-header", "--csv", "time,mti,11", "src/test/resources/basic.xml")
+    val output = run("--no-header", "--csv", "time,mti,11", testFile("basic.xml"))
     output shouldEqual """00:00:03.292,0800,28928
                          |13:10:55.000,,
                          |""".stripMargin
   }
 
   it should "filter on exact field match with -f option" in {
-    val output = run("-f", "11=28928", "src/test/resources/basic.xml")
+    val output = run("-f", "11=28928", testFile("basic.xml"))
     output shouldEqual """<log realm="some.channel/10.0.0.1:4321" at="Mon Nov 24 00:00:03 EST 2014.292" lifespan="10005ms">
                          |  <receive>
                          |    <isomsg direction="incoming">
@@ -160,7 +161,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "produce pair entries with --pair option" in {
-    val output = run("--pair", "--csv", "time,mti,11,rtt", "src/test/resources/a-bunch.xml")
+    val output = run("--pair", "--csv", "time,mti,11,rtt", testFile("a-bunch.xml"))
     // output ends up in ordered by response received time...
     output shouldEqual """time,mti,11,rtt
                          |00:00:04.292,0200,2,600
@@ -169,7 +170,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "sort by --sort option" in {
-    val output = run("--sort", "time", "--pair", "--csv", "time,mti,11,rtt", "src/test/resources/a-bunch.xml")
+    val output = run("--sort", "time", "--pair", "--csv", "time,mti,11,rtt", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,rtt
                          |00:00:03.292,0200,1,1700
                          |00:00:04.292,0200,2,600
@@ -177,7 +178,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "reverse sort with --sort-desc option" in {
-    val output = run("--sort-desc", "11", "--pair", "--csv", "time,mti,11,rtt", "src/test/resources/a-bunch.xml")
+    val output = run("--sort-desc", "11", "--pair", "--csv", "time,mti,11,rtt", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,rtt
                          |00:00:04.292,0200,2,600
                          |00:00:03.292,0200,1,1700
@@ -185,28 +186,28 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "filter using greater than operator with -f rtt>1000 option" in {
-    val output = run("--pair", "-f", "rtt>1000", "--csv", "time,mti,11,rtt", "src/test/resources/a-bunch.xml")
+    val output = run("--pair", "-f", "rtt>1000", "--csv", "time,mti,11,rtt", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,rtt
                          |00:00:03.292,0200,1,1700
                          |""".stripMargin
   }
 
   it should "filter using less than operator with -f rtt<1000 option" in {
-    val output = run("--pair", "-f", "rtt<1000", "--csv", "time,mti,11,rtt", "src/test/resources/a-bunch.xml")
+    val output = run("--pair", "-f", "rtt<1000", "--csv", "time,mti,11,rtt", testFile("a-bunch.xml"))
     output shouldEqual """time,mti,11,rtt
                          |00:00:04.292,0200,2,600
                          |""".stripMargin
   }
 
   it should "filter using case insensitive contains operator with -f 48.1~suBfield option" in {
-    val output = run("-f", "48.1~suBfield", "--csv", "time,mti,11,file", "src/test/resources/a-pair.xml")
+    val output = run("-f", "48.1~suBfield", "--csv", "time,mti,11,file", testFile("a-pair.xml"))
     output shouldEqual """time,mti,11,file
                          |00:00:04.100,0810,28928,a-pair.xml:14
                          |""".stripMargin
   }
 
   it should "negate filter with !in -f 24!=831 option" in {
-    val output = run("-f", "24!=831", "src/test/resources/a-pair.xml")
+    val output = run("-f", "24!=831", testFile("a-pair.xml"))
     output shouldEqual """<log realm="rotate-log-listener" at="Mon Nov 24 13:10:55 EST 2014">
                          |   maxSize (50000000) threshold reached
                          |</log>
@@ -214,7 +215,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "match anywhere in the message with -grep option" in {
-    val output = run("--grep", "threshold", "src/test/resources/a-pair.xml")
+    val output = run("--grep", "threshold", testFile("a-pair.xml"))
     output shouldEqual """<log realm="rotate-log-listener" at="Mon Nov 24 13:10:55 EST 2014">
                          |   maxSize (50000000) threshold reached
                          |</log>
@@ -222,7 +223,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "negate --grep with --grep! option" in {
-    val output = run("--grep!", "threshold", "src/test/resources/a-pair.xml")
+    val output = run("--grep!", "threshold", testFile("a-pair.xml"))
     output shouldEqual """<log realm="some.channel/10.0.0.1:4321" at="Mon Nov 24 00:00:03 EST 2014.292" lifespan="10005ms">
                          |  <send>
                          |    <isomsg direction="outgoing">
@@ -252,7 +253,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "print full text by default" in {
-    val output = run("src/test/resources/a-pair.xml")
+    val output = run(testFile("a-pair.xml"))
     output shouldEqual """<log realm="some.channel/10.0.0.1:4321" at="Mon Nov 24 00:00:03 EST 2014.292" lifespan="10005ms">
                          |  <send>
                          |    <isomsg direction="outgoing">
@@ -285,7 +286,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "include N messages preceding a match with -B N option" in {
-    val output = run("-B", "1", "--csv", "48.1", "--no-header", "--grep", "#3", "src/test/resources/a-bunch.xml")
+    val output = run("-B", "1", "--csv", "48.1", "--no-header", "--grep", "#3", testFile("a-bunch.xml"))
     output shouldEqual
       """a-bunch.xml #2
         |a-bunch.xml #3
@@ -293,7 +294,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "include N messages following a match with -A N option" in {
-    val output = run("-A", "1", "--csv", "48.1", "--no-header", "--grep", "#2", "src/test/resources/a-bunch.xml")
+    val output = run("-A", "1", "--csv", "48.1", "--no-header", "--grep", "#2", testFile("a-bunch.xml"))
     output shouldEqual
       """a-bunch.xml #2
         |a-bunch.xml #3
@@ -301,7 +302,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "include N messages before and after a match with -C N option" in {
-    val output = run("-C", "1", "--csv", "48.1", "--no-header", "--grep", "#3", "src/test/resources/a-bunch.xml")
+    val output = run("-C", "1", "--csv", "48.1", "--no-header", "--grep", "#3", testFile("a-bunch.xml"))
     output shouldEqual
       """a-bunch.xml #2
         |a-bunch.xml #3
@@ -310,7 +311,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "includes preceding context only once with -B N option" in {
-    val output = run("-B", "1", "--csv", "48.1", "--no-header", "--field", "at~00:00:04", "src/test/resources/a-bunch.xml")
+    val output = run("-B", "1", "--csv", "48.1", "--no-header", "--field", "at~00:00:04", testFile("a-bunch.xml"))
     output shouldEqual
       """a-bunch.xml #1
         |a-bunch.xml #2
@@ -320,14 +321,14 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "include records matching all -f filters" in {
-    val output = run("-f", "48.1~bunch", "-f", "at~.892", "--csv", "48.1", "--no-header", "src/test/resources/a-bunch.xml")
+    val output = run("-f", "48.1~bunch", "-f", "at~.892", "--csv", "48.1", "--no-header", testFile("a-bunch.xml"))
     output shouldEqual
       """a-bunch.xml #3
         |""".stripMargin
   }
 
   it should "print wrap pairs in <pair></pair> with --pair" in {
-    val output = run("--pair","src/test/resources/a-pair.xml")
+    val output = run("--pair",testFile("a-pair.xml"))
     output shouldEqual """<pair>
                          |<log realm="some.channel/10.0.0.1:4321" at="Mon Nov 24 00:00:03 EST 2014.292" lifespan="10005ms">
                          |  <send>
@@ -359,7 +360,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "filter on lifespan greater than a number" in {
-    val output = run("--field", "lifespan>1001", "--csv", "mti,lifespan", "src/test/resources/a-pair.xml")
+    val output = run("--field", "lifespan>1001", "--csv", "mti,lifespan", testFile("a-pair.xml"))
     output shouldBe
       """mti,lifespan
         |0800,10005
@@ -367,7 +368,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   it should "filter on lifespan less than a number" in {
-    val output = run("--field", "lifespan<1000", "--csv", "mti,lifespan", "src/test/resources/a-pair.xml")
+    val output = run("--field", "lifespan<1000", "--csv", "mti,lifespan", testFile("a-pair.xml"))
     output shouldBe
       """mti,lifespan
         |0810,1000
@@ -376,7 +377,7 @@ class MainTest extends FlatSpec with Matchers {
   }
 
   "Given a <log> record garbled by a 2nd partial within it" should "keep on truckin'" in {
-    val output = run("--csv", "0,11", "src/test/resources/message-inception-garbage.xml")
+    val output = run("--csv", "0,11", testFile("message-inception-garbage.xml"))
     output shouldEqual """0,11
                           |2804,
                           |2814,127
@@ -385,7 +386,7 @@ class MainTest extends FlatSpec with Matchers {
 
   "With --strict garbled input" should "be rejected" in {
     the [IllegalArgumentException] thrownBy {
-      run("--strict", "src/test/resources/message-inception-garbage.xml")
+      run("--strict", testFile("message-inception-garbage.xml"))
     } should have message """Unexpected <log> start tag. Line message-inception-garbage.xml:5:             <field id="0" value="2804"/><log realm="remoteLink.channel" at="Mon Dec 08 14:51:55 EST 2014.648">"""
   }
 
