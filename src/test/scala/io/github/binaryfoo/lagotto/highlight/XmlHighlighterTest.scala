476762ec3b1e35cab6cf14dbfa759a22dd1832c5
Will Currie <will@currie.id.au>
2015-03-16 06:08:20 +1100

    Less wordy.

--- src/test/scala/io/github/binaryfoo/lagotto/highlight/XmlHighlighterTest.scala
+++ src/test/scala/io/github/binaryfoo/lagotto/highlight/XmlHighlighterTest.scala
@@ -3,30 +3,31 @@ package io.github.binaryfoo.lagotto.highlight
 import java.io.{FileWriter, PrintWriter, StringWriter}
 
 import io.github.binaryfoo.lagotto.LagoTest
+import io.github.binaryfoo.lagotto.highlight.XmlHighlighter.highlight
 
 class XmlHighlighterTest extends LagoTest {
 
   "ANSI output format" should "highlight" in {
     val xml = contentsOf("one.xml")
-    val highlighted = XmlHighlighter.highlight(xml, AnsiMarkup)
+    val highlighted = highlight(xml, AnsiMarkup)
     highlighted shouldBe contentsOf("expected-ansi-one.txt").dropRight(1) // drop trailing newline
   }
 
   "HTML output format" should "highlight" in {
     val xml = contentsOf("one.xml")
-    val highlighted = XmlHighlighter.highlight(xml, HtmlMarkup)
+    val highlighted = highlight(xml, HtmlMarkup)
     highlighted shouldBe contentsOf("expected-html-one.html").dropRight(1) // drop trailing newline
   }
 
   "Null output format" should "do nothing" in {
     val xml = contentsOf("one.xml")
-    val highlighted = XmlHighlighter.highlight(xml, NotMarkedUp)
+    val highlighted = highlight(xml, NotMarkedUp)
     highlighted shouldBe contentsOf("one.xml").dropRight(1) // drop trailing newline
   }
 
   it should "handle just a start tag" in {
     val xml = """<log realm="terminal-server-1-newCA.server.channel/203.39.124.193:22740" at="Wed Dec 10 13:56:24 EST 2014.106">"""
-    val highlighted = XmlHighlighter.highlight(xml, NotMarkedUp)
+    val highlighted = highlight(xml, NotMarkedUp)
     highlighted shouldBe xml
   }
 }
